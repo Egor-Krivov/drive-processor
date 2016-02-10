08974d29b1f10a306a06a6e042e449a890d2efcb
Egor Krivov <egor.krivov@frtk.ru>
2016-02-10 12:52:07 +0300

    moved

--- dp.py
+++ dp.py
@@ -10,12 +10,15 @@ class EventHandler(pyinotify.ProcessEvent):
     def process_IN_DELETE(self, event):
         print("Removing:", event.pathname)
 
+    def process_IN_MOVED_TO(self, event):
+        print("Moved to:", event)
+
 
 wm = pyinotify.WatchManager()  # Watch Manager
-mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_ISDIR
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MOVED_TO
 
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch('./data', mask, rec=True, auto_add=True)
+wdd = wm.add_watch('/media/data/Dropbox/test', mask, rec=True, auto_add=True)
 
 notifier.loop()
